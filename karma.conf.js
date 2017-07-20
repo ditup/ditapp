044eae33297c4471a1dbbbf4846abd315139cf6f
mrkvon <michal.salajka@protonmail.com>
2017-07-20 17:27:33 +0200

    changed karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      // require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
     ],
@@ -37,7 +38,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [/*'progress', 'kjhtml'*/, 'mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
