c42b08e6c414a335bdcf7f41f067a5c61af65b46
mrkvon <michal.salajka@protonmail.com>
2017-10-09 23:49:04 +0200

    improve editor
    - style user input and output
    - sanitize content of medium-editor
    - format user output in tag-remove-confirm.component

--- src/app/shared/editor/editor.component.ts
+++ src/app/shared/editor/editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, SecurityContext } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as MediumEditor from 'medium-editor';
 
 @Component({
@@ -11,24 +12,38 @@ export class EditorComponent implements OnInit, OnChanges, OnDestroy {
   @Input() form: FormGroup;
   @Input() value = '';
   @Input() formControlName: string;
+  @Input() placeholder = 'Type your text';
   /* Is the component position:fixed? (ensure correct position of medium-editor) */
   @Input() fixed = false;
 
   editor: any;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
 
     // set the options
-    const options: any = { };
+    const options: any = {
+      toolbar: {
+        buttons: [ 'bold', 'italic', 'underline', 'anchor', 'quote'],
+        standardizeSelectionStart: true,
+      },
+      placeholder: {
+        text: this.placeholder
+      },
+      paste: {
+        cleanPastedHTML: true
+      },
+      autoLink: true,
+      imageDragging: false
+    };
     if (this.fixed) {
       options.elementsContainer = this.el.nativeElement.parentNode;
     }
 
     // initialize the medium-editor
     this.editor = new MediumEditor(this.el.nativeElement, options);
-    this.editor.setContent(this.value);
+    this.setEditorContent(this.value);
 
     // when content changes, update the hidden textarea, too
     this.editor.subscribe('editableInput', (_event, editable) => {
@@ -40,7 +55,7 @@ export class EditorComponent implements OnInit, OnChanges, OnDestroy {
     // when value changes, update the editor content
     if (changes.value && changes.value.currentValue !== changes.value.previousValue) {
       if (this.editor) {
-        this.editor.setContent(this.value);
+        this.setEditorContent(this.value);
       }
     }
   }
@@ -53,6 +68,11 @@ export class EditorComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   clear() {
-    this.editor.setContent('');
+    this.setEditorContent('');
+  }
+
+  private setEditorContent(value: string) {
+    const sanitizedValue: SafeHtml = this.sanitizer.sanitize(SecurityContext.HTML, value);
+    this.editor.setContent(sanitizedValue);
   }
 }
