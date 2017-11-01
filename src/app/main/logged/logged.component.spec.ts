e34a8c13222362e12bda7781b48bd6bb672b7fb3
mrkvon <michal.salajka@protonmail.com>
2017-11-01 05:15:03 +0100

    change main logged page

--- src/app/main/logged/logged.component.spec.ts
+++ src/app/main/logged/logged.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoggedComponent } from './logged.component';
+
+import { MaterialModule } from '../../material.module';
 
 describe('LoggedComponent', () => {
   let component: LoggedComponent;
@@ -8,7 +11,11 @@ describe('LoggedComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoggedComponent ]
+      declarations: [ LoggedComponent ],
+      imports: [
+        MaterialModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/main/logged/logged.component.ts
+++ src/app/main/logged/logged.component.ts
@@ -7,6 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoggedComponent implements OnInit {
 
+  public exploreOptions: any[] = [
+    {
+      link: '/profile/edit',
+      icon: 'person',
+      title: 'about me'
+    },
+    {
+      link: '/profile/edit/tags',
+      icon: 'favorite',
+      title: 'i care about...'
+    },
+    {
+      link: '/people',
+      icon: 'people',
+      title: 'people alike'
+    },
+    {
+      link: '/map',
+      icon: 'person_pin_circle',
+      title: 'people around'
+    },
+    {
+      todo: true,
+      link: '/ideas',
+      icon: 'lightbulb_outline',
+      title: 'ideas'
+    },
+    {
+      todo: true,
+      link: '/challenges',
+      icon: 'extension',
+      title: 'challenges'
+    },
+    {
+      todo: true,
+      link: '/projects',
+      icon: 'build',
+      title: 'projects'
+    },
+    {
+      todo: true,
+      link: '/discussions',
+      icon: 'forum',
+      title: 'discussions'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
